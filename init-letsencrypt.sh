705ee3c6e2ff515504e9c38549e853a4e17425ef
AlexPepl <alexandrospeponis1999@gmail.com>
2025-07-12 13:23:28 +0300

    main

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,8 +3,8 @@
 domains=(pepsmonitor.com www.pepsmonitor.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="alexandrospeponis1999@gmail.com" # Change this!
-staging=0 # Set to 1 for testing, 0 for real
+email="alexandrospeponis1999@gmail.com" 
+staging="" 
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found. Continue and replace existing certificate? (y/N) " decision
@@ -20,4 +20,4 @@ docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
   --email $email --agree-tos --no-eff-email \
   -d pepsmonitor.com -d www.pepsmonitor.com \
-  ${staging:+--staging}" certbot
+  $staging" certbot
