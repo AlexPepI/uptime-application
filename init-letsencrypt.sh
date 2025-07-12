073dd44261dfe668af2efdfc0e26da6d048acfa1
AlexPepl <alexandrospeponis1999@gmail.com>
2025-07-12 12:02:41 +0300

    Nginx SSL

---
+++ init-letsencrypt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+domains=(pepsmonitor.com www.pepsmonitor.com)
+rsa_key_size=4096
+data_path="./data/certbot"
+email="alexandrospeponis1999@gmail.com" # Change this!
+staging=0 # Set to 1 for testing, 0 for real
+
+if [ -d "$data_path" ]; then
+  read -p "Existing data found. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+mkdir -p "$data_path/www"
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  mkdir -p /var/www/certbot" nginx
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+  --email $email --agree-tos --no-eff-email \
+  -d pepsmonitor.com -d www.pepsmonitor.com \
+  ${staging:+--staging}" certbot
