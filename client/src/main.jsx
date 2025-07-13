be79958521ed401c2345e4e043ea0bf4a78496db
AlexPepl <alexandrospeponis1999@gmail.com>
2025-07-13 21:50:50 +0300

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -19,7 +19,7 @@ createRoot(document.getElementById('root')).render(
      appearance={{baseTheme:dark}}
     >
       <ClerkLoading>
-        LOADING ..
+        LOADING ...
       </ClerkLoading>
       <ClerkLoaded>
         <App />  
