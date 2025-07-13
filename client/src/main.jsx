26fce23a4d01ff6a1f3981c4f20a4be955d80d6d
AlexPepl <alexandrospeponis1999@gmail.com>
2025-07-13 20:46:56 +0300

    dotenv

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
      afterSignOutUrl='/'
     >
       <ClerkLoading>
-        LOADING ...
+        LOADING ..
       </ClerkLoading>
       <ClerkLoaded>
         <App />  
