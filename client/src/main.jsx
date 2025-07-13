9b038917d43ee7d3770cc1b00c10986376ea87d2
AlexPepl <alexandrospeponis1999@gmail.com>
2025-07-13 21:48:55 +0300

    Dark Mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import './index.css'
 import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {dark} from "@clerk/themes"
 import { ClerkProvider,ClerkLoaded,ClerkLoading } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
     <ClerkProvider
      publishableKey={PUBLISHABLE_KEY} 
      afterSignOutUrl='/'
+     appearance={{baseTheme:dark}}
     >
       <ClerkLoading>
         LOADING ..
