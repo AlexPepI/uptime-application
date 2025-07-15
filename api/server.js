9441d2db9eb731e332cb067c1601c55fab3890cb
AlexPepl <alexandrospeponis1999@gmail.com>
2025-07-15 18:30:31 +0300

    WebHook

--- api/server.js
+++ api/server.js
@@ -3,14 +3,17 @@ import cors from "cors";
 import express from "express";
 import sync from "./config/sync.js";
 import userRouter from "./user/user.router.js";
+import clerkWebhook from "./webhooks/clerk/clerkWebhook.js"
 // import uptimeRouter from "./uptime/uptime.router.js";
 import { clerkMiddleware, requireAuth } from '@clerk/express';
 
 const app = express();
+app.use(cors());
+app.use(clerkWebhook);
 app.use(express.json());
 const port = 3001;
 
-app.use(cors());
+
 app.use(clerkMiddleware());
 sync();
 
---
+++ api/webhooks/clerk/clerkWebhook.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import User from '../../models/User.js';
+import express from 'express';
+import { Webhook } from 'svix';
+
+dotenv.config();
+const clerkWebhookRouter = express.Router();
+
+clerkWebhookRouter.post(
+  '/api/webhooks',
+  express.raw({ type: 'application/json' }),
+  async (req, res) => {
+    try {
+      const secret = process.env.CLERK_WEBHOOK_SECRET;
+      const wh = new Webhook(secret);
+      const evt = wh.verify(req.body, req.headers);
+
+      if (evt.type === 'user.created') {
+        const clerkId = evt.data.id;
+        await User.create({
+            clerk_Id:clerkId
+        })
+      }
+      res.status(200).send('Webhook received');
+    } catch (err) {
+      res.status(400).send('Invalid webhook');
+    }
+  }
+);
+
+export default clerkWebhookRouter;
+
